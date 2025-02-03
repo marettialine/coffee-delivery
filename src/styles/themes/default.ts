328dbd66c73ffe174f42a853c97712f2242e0952
marettialine <alinesmaretti@gmail.com>
2025-02-03 19:04:37 -0300

    fix: update colors in default themes

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -4,7 +4,7 @@ export const defaultTheme = {
   background: '#FAFAFA', // gray-100
   'base-card': '#F3F2F2', // gray-200
   'base-input': '#EDEDED', // gray-300
-  'base-button': '#E6E6E5', // gray-400
+  'base-button': '#E6E5E5', // gray-400
   'base-hover': '#D7D5D5', // gray-500
   'base-label': '#8D8686', // gray-600
   'base-text': '#574F4D', // gray-700
